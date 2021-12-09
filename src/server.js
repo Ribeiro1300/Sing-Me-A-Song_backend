889210c7688b69fb5558f3a8a614967f23cafa06
Bruna Topan <brutopanb@gmail.com>
2021-12-09 16:55:55 -0300

    Preparando review

--- src/server.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import "./setup.js";
-import app from "./app.js";
-
-app.listen(process.env.PORT, () => {
-	console.log("Server running on port " + process.env.PORT);
-	console.log("database url " + process.env.DATABASE_URL);
-});
